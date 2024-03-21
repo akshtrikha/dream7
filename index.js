e47813b40c781a133e9890d659a6954ebff2d4ae
Akshit Trikha <akshittrikha01@gmail.com>
2024-03-22 02:30:28 +0530

    Add: basic api server with GET/match/:id and GET/result Add: logic to get the player points

---
+++ index.js
@@ -0,0 +1,96 @@
+import express from "express";
+import axios from "axios";
+
+import { batsmenPointsCard, bowlerPointsCard } from "./utils/score-engine.js";
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(express.json());
+
+// app.get("/teams", async (req, res) => {
+//   const matchId = req.query.matchId;
+//   console.log("matchId: " + matchId);
+
+//   const options = {
+//     method: "GET",
+//     url: `https://cricbuzz-cricket.p.rapidapi.com/mcenter/v1/${matchId}`,
+//     headers: {
+//       "X-RapidAPI-Key": "a77584eb69msh69fb7d597d14f23p16e353jsn16a8a0f9a186",
+//       "X-RapidAPI-Host": "cricbuzz-cricket.p.rapidapi.com",
+//     },
+//   };
+
+//   try {
+//     const response = await axios.request(options);
+//     // console.log(response.data);
+//     return res.status(200).json(response.data.);
+//   } catch (error) {
+//     console.error(error);
+//     return res.status(400).json({ error });
+//   }
+// });
+
+app.get("/match/:id", async (req, res) => {
+  const matchId = req.params.id;
+  console.log(`/GET match/${matchId}`);
+
+  const options = {
+    method: "GET",
+    url: `https://cricbuzz-cricket.p.rapidapi.com/mcenter/v1/${matchId}`,
+    headers: {
+      "X-RapidAPI-Key": "a77584eb69msh69fb7d597d14f23p16e353jsn16a8a0f9a186",
+      "X-RapidAPI-Host": "cricbuzz-cricket.p.rapidapi.com",
+    },
+  };
+
+  try {
+    const response = await axios.request(options);
+    // console.log(response.data);
+    return res.status(200).json(response.data);
+  } catch (error) {
+    console.error(error);
+    return res.status(400).json({ error });
+  }
+});
+
+app.get("/result", async (req, res) => {
+  console.log("/GET result");
+
+  const matchId = req.query.matchId;
+  console.log(matchId);
+  const options = {
+    method: "GET",
+    url: `https://cricbuzz-cricket.p.rapidapi.com/mcenter/v1/${matchId}/hscard`,
+    headers: {
+      "X-RapidAPI-Key": "a77584eb69msh69fb7d597d14f23p16e353jsn16a8a0f9a186",
+      "X-RapidAPI-Host": "cricbuzz-cricket.p.rapidapi.com",
+    },
+  };
+
+  try {
+    const response = await axios.request(options);
+    // console.log(response.data);
+
+    const matchData = {
+      teamData1: response.data.scoreCard[0],
+      teamData2: response.data.scoreCard[1],
+    };
+
+    const batsmenPoints = batsmenPointsCard(
+      matchData.teamData1.batTeamDetails.batsmenData
+    );
+    const bowlerPoints = bowlerPointsCard(
+      matchData.teamData1.bowlTeamDetails.bowlersData
+    );
+
+    res.status(200).json(matchData);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send(error);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on ${port}`);
+});
---
+++ utils/score-engine.js
@@ -0,0 +1,118 @@
+export const scoreRules = {
+  batting: {
+    run: 1,
+    boundaryBonus: 1,
+    sixBonus: 2,
+    thirtyRunBonus: 4,
+    halfCenturyBonus: 8,
+    centuryBonus: 16,
+    dismissalForDuck: -2,
+  },
+  bowling: {
+    wicket: 25,
+    bonus: 8, // LBW/Bowled
+    threeWicketBonus: 4,
+    fourWicketBonus: 8,
+    fiveWicketBonus: 16,
+    maidenOver: 12,
+  },
+  fieldingPoints: {
+    catch: 8,
+    threeCatchBonus: 4,
+    stumping: 12,
+    runOutDirectHit: 12,
+    runOutNotADirectHit: 6,
+  },
+  others: {
+    //! Count as factor
+    captain: 2,
+    viceCaptain: 1.5,
+    inAnnouncedLineups: 4,
+    playingSubstitute: 4, //! Concussion, COVID-19, X-Factor, or Impact Player
+  },
+  economyRatePoints: {
+    below5: 6, // below 5
+    below6: 4, // between 5 and 5.99
+    below7: 2, // between 7 and 7
+    above10: -2, // between 10 and 11
+    above11: -4, // between 11.01 and 12
+    above12: -6, // above 12
+  },
+  strikeRate: {
+    //! Min. 10 balls played. Per 100 balls
+    above170: 6, // above 170
+    above150: 4, // between 150.01 nd 170
+    above130: 2, // between 130-150
+    below70: -2, // between 60-70
+    below60: -4, // between 50-50.99
+    below50: -6, // below 50
+  },
+};
+
+export const batsmenPointsCard = function (batsmenData) {
+  // teamData1.batTeamDetails.batsmenData
+  let pointsCard = [];
+  for (const batsmanKey in batsmenData) {
+    const batsman = batsmenData[batsmanKey];
+    const runs = batsman.runs;
+    const fours = batsman.fours;
+    const sixes = batsman.sixes;
+    const ballsFaced = batsman.balls;
+    const strikeRate = batsman.strikeRate;
+
+    let points =
+      runs +
+      fours * scoreRules.batting.boundaryBonus +
+      sixes * scoreRules.batting.sixBonus;
+
+    if (runs >= 100) points += scoreRules.batting.centuryBonus;
+    else if (runs >= 50) points += scoreRules.batting.halfCenturyBonus;
+    else if (runs >= 30)
+      points += scoreRules.batting.thirtyRunBonusutils / score - engine.js;
+    else if (runs === 0) points -= scoreRules.batting.dismissalForDuck;
+
+    if (ballsFaced >= 10) {
+      if (strikeRate >= 170) points += scoreRules.strikeRate.above170;
+      else if (strikeRate >= 150) points += scoreRules.strikeRate.above150;
+      else if (strikeRate >= 130) points += scoreRules.strikeRate.above130;
+      else if (strikeRate <= 50) points += scoreRules.strikeRate.below50;
+      else if (strikeRate <= 60) points += scoreRules.strikeRate.below60;
+      else if (strikeRate <= 70) points += scoreRules.strikeRate.below70;
+    }
+    // console.log(points)
+
+    pointsCard.push(points);
+  }
+
+  // console.log(pointsCard)
+  return pointsCard;
+};
+
+export const bowlerPointsCard = function (bowlerData) {
+  let pointsCard = [];
+  for (const bowlerKey in bowlerData) {
+    const bowler = bowlerData[bowlerKey];
+    const wickets = bowler.wickets;
+    const maidens = bowler.maidens;
+    const economy = bowler.economy;
+
+    let points = wickets * scoreRules.bowling.wicket;
+    if (wickets >= 5) points += scoreRules.bowling.fiveWicketBonus;
+    else if (wickets >= 4) points += scoreRules.bowling.fourWicketBonus;
+    else if (wickets >= 3) points += scoreRules.bowling.threeWicketBonus;
+
+    points += maidens * scoreRules.bowling.maidenOver;
+
+    if (economy >= 12) points += scoreRules.economyRatePoints.above12;
+    else if (economy >= 11) points += scoreRules.economyRatePoints.above11;
+    else if (economy >= 10) points += scoreRules.economyRatePoints.above10;
+    else if (economy < 5) points += scoreRules.economyRatePoints.below5;
+    else if (economy < 6) points += scoreRules.economyRatePoints.below6;
+    else if (economy < 7) points += scoreRules.economyRatePoints.below7;
+
+    pointsCard.push(points);
+  }
+
+  // console.log(pointsCard)
+  return pointsCard;
+};
